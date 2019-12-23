87d29598f546df932ce296027a8b2dd28c24bc3c
Виктор Антонович <bvictor@belous.su>
2019-12-23 18:58:39 +0300

    debug

--- ficop3.c
+++ ficop3.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 
 /* copy input to output; 1st version */
+/* exercize 1-8 */
 
 int main()
 {
 	int c;
-	int oldc;
+	int blanks = 0;
+	int tabs = 0;
+	int newlines = 0;
 
 	while ((c = getchar()) !=EOF) {
+		if (c = ' ') ++blanks;
+		if (c = '\n') ++ newlines;
+		if (c = '\t') ++ tabs
 		putchar(c);
 	}
-	printf("%d\n", c);
+	printf("blanks = %d, newlines = %d, tabs = %d\n", blanks,newlines,tabs);
 }
