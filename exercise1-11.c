198e8116c886843e1e4a4deb56d6c3ee2bebc028
Виктор Антонович <bvictor@belous.su>
2019-12-25 10:21:20 +0300

    error4

--- exercise1-11.c
+++ exercise1-11.c
@@ -20,9 +20,9 @@ int main()
 				state = IN;
 				++nw;
 			};
-			if (c == ' ' !! c == '\t')
+			if ((c == ' ' ) !! (c == '\t'))
 				putchar('\n');
-			if (c != ' ' !! c != '\t')	putchar(c);
+			if ((c != ' ') !! (c != '\t'))	putchar(c);
 	}
 	printf("%d, %d, %d\n", nl, nw, nc);
 }
